339deff5a24e7d3cc6b5692ad0ed8e0317a67222
Karol Marcjan <karol.marcjan@gmail.com>
2012-05-22 22:25:36 +0200

    1. Made git ignore Vim swapfiles.
    2.Started working on precalculating the probability ladders.

--- kmc/gdmodel/base.py
+++ kmc/gdmodel/base.py
@@ -1,9 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import copy
+
 import numpy
+
+
+def distance(p_1, p_2):
+    """ Calculate the distance between two given points. """
+    return numpy.sqrt(numpy.sum((p_1 - p_2)**2))
 
 class Polymer(object):
     def __init__(self, dynamics_class, link_length, reptons,  *args, **kwargs):
         """ Initialize polymer object. 
-        
+
         Arguments:
         - dynamics_class: represents polymer dynamics
         - link_length: the maxmimum repton to repton distance
@@ -16,17 +25,44 @@ class Polymer(object):
         # `self.reptons` matrix
         self.positions = numpy.zeros((reptons, self.dynamics.get_dim()))
         # let's randomize the positions
-        self._randomize_reptons()
+        self.__randomize_reptons()
 
 
-    def _randomize_reptons(self):
+    def __randomize_reptons(self):
         """ Randomizes repton positions """
         for position in range(1, self.positions.shape[0]):
             trans = self.dynamics.select_translation(prev_trans=None)
             t_vect = self.dynamics.translations.get(trans)
             self.positions[position] = self.positions[position-1] + t_vect
 
+    # Not working yet! That's what you get for mangling with other people's code
+    # and multiple for loops.
+    def __precalculate_ladders(self):
+        """ Precalculate probability ladders for a given translation. """
+
+        self.ladders = {}
+        translations = self.dynamics.get_all_translations()
+
+        rel_positions = copy.copy(translations)
+        rel_positions['same'] = numpy.array([0, 0])
+
+        for dr_key, dr in translations.items():
+            self.ladders[dr_key] = {}
+
+            for init_dr_key, init_dr in rel_positions.items():
+
+                self.ladders[dr_key][init_dr_key] = copy.copy(translations)
+
+                for pulled_dr_key, pulled_dr in\
+                    self.ladders[dr_key][init_dr_key].items():
+
+                    if distance(pulled_dr, init_dr + dr) > self.dynamics.link_length:
+
+                        del self.ladders[dr_key][init_dr_key]
+
+
 class Translation(object):
+    """Semi-abstract base class for possible translations sets."""
 
     def __init__(self, link_length, *args, **kwargs):
         self.link_length = link_length
@@ -65,7 +101,7 @@ class Translation(object):
         raise NotImplementedError
 
 class SquareTranslation(Translation):
-    
+
     def get_all_translations(self):
         return {'up': numpy.array((0, 1)),
                 'right': numpy.array((1, 0)),
@@ -74,6 +110,7 @@ class SquareTranslation(Translation):
 
 
     def get_dim(self):
+        # Why use the get method instead of square brackets here?
         return self.get_all_translations().get('up').size
 
     def rate_up(self, *args, **kwargs):
@@ -87,9 +124,9 @@ class SquareTranslation(Translation):
 
     def rate_down(self, *args, **kwargs):
         return 0.25
-    
+
 if __name__ == "__main__":
     # Jus a test..
     p = Polymer(SquareTranslation, 1.0, 40)
     for x, y in p.positions:
-        print x, "\t", y
+        print "%.2f\t%.2f" % (x, y)
